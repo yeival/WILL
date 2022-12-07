a6b34dffb022ce74b837209333291c961d6654eb
Yeison Valdeblanquez <yeival@gasguajira.local>
2022-12-06 20:50:29 -0500

    Cambio 1

--- 01.js
+++ 01.js
@@ -11,7 +11,14 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+  var nuevoarray = [];
+   for(var i = 0; i < array.length; i++)
+    {
+     if(!isNaN(array[i]))  
+       nuevoarray.push(array[i]);
+    }
+    return nuevoarray;
+  
 }
 
 // No modifiques nada debajo de esta linea //
